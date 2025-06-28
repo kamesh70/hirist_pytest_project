f0f806baa80dafec8d006bb376267dcb5d7323b2
kamesh <kamesh141095@gmail.com>
2025-06-28 15:05:57 +0530

    notification test add

--- tests/test_detail.py
+++ tests/test_detail.py
@@ -9,7 +9,15 @@ class TestDetails:
     @pytest.fixture(autouse=True)
     def setup_detail_page(self, logged_in_driver):
         self.detail = DetailPage(logged_in_driver)
+        self.detail.wait_for_page_load()
 
     @pytest.mark.parametrize("text",["Search for Jobs, Companies, Courses"])
     def test_detail_flow(self,text):
         assert text == self.detail.search_fun()
+
+    @pytest.mark.parametrize("text",["Notifications"])
+    def test_notification(self,text):
+       assert text == self.detail.Notification_fun()
+
+    def test_show_all_notification(self):
+        self.detail.click_show_all_notification()
